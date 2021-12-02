a45968c8d1980330e46744c520f1e9c5c360ec94
elcaskerito <idimhtt4t@gmail.com>
2021-12-02 12:48:30 +0100

    mis-a-jour du script 'BD_informatique' en inserant quelque données dans la base de donnée

---
+++ hh.py
@@ -0,0 +1,3 @@
+jkdhsjkhs
+jjjj
+sf
--- script_DB/BD_Informatique.sql
+++ script_DB/BD_Informatique.sql
@@ -1,98 +1,258 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : jeu. 02 déc. 2021 à 12:39
+-- Version du serveur :  8.0.27-0ubuntu0.20.04.1
+-- Version de PHP : 7.4.3
 
-CREATE SCHEMA IF NOT EXISTS `BD_Informatique` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `BD_Informatique` ;
-
--- -----------------------------------------------------
--- Table `BD_Informatique`.`Utilisateur`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `BD_Informatique`.`Utilisateur` (
-  `idUtilisateur` INT NOT NULL AUTO_INCREMENT ,
-  `nom` VARCHAR(45) NULL ,
-  `prenom` VARCHAR(45) NULL ,
-  `email` VARCHAR(45) NULL ,
-  `role` VARCHAR(45) NULL ,
-  `login` VARCHAR(45) NULL ,
-  `password` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idUtilisateur`) )
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `BD_Informatique`.`role`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `BD_Informatique`.`role` (
-  `idrole` INT NOT NULL AUTO_INCREMENT ,
-  `nomRole` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idrole`) )
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données : `BD_Informatique`
+--
 
--- -----------------------------------------------------
--- Table `BD_Informatique`.`client`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `BD_Informatique`.`client` (
-  `idclient` INT NOT NULL AUTO_INCREMENT ,
-  `nom` VARCHAR(45) NULL ,
-  `prenom` VARCHAR(45) NULL ,
-  `tel` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idclient`) )
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `client`
+--
 
--- -----------------------------------------------------
--- Table `BD_Informatique`.`produit`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `BD_Informatique`.`produit` (
-  `idproduit` INT NOT NULL AUTO_INCREMENT ,
-  `nomProduit` VARCHAR(45) NULL ,
-  `dateDexpiration` DATE NULL ,
-  `typeDeProduit` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idproduit`) )
-ENGINE = InnoDB;
+CREATE TABLE `client` (
+  `idclient` int NOT NULL,
+  `nom` varchar(45) DEFAULT NULL,
+  `prenom` varchar(45) DEFAULT NULL,
+  `tel` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
+--
+-- Déchargement des données de la table `client`
+--
 
--- -----------------------------------------------------
--- Table `BD_Informatique`.`stock`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `BD_Informatique`.`stock` (
-  `idstock` INT NOT NULL AUTO_INCREMENT ,
-  `nomDuProduit` VARCHAR(45) NULL ,
-  `quantité` VARCHAR(45) NULL ,
-  `dateDajout` VARCHAR(45) NULL ,
-  PRIMARY KEY (`idstock`) )
-ENGINE = InnoDB;
+INSERT INTO `client` (`idclient`, `nom`, `prenom`, `tel`) VALUES
+(1, 'idris', 'mahamat', '66899504');
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `BD_Informatique`.`facture`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `BD_Informatique`.`facture` (
-  `idfacture` INT NOT NULL AUTO_INCREMENT ,
-  `designation` VARCHAR(45) NULL ,
-  `quantité` FLOAT NULL ,
-  `prixUnitaire` FLOAT NULL ,
-  `prixTotal` FLOAT NULL ,
-  `Total` FLOAT NULL ,
-  PRIMARY KEY (`idfacture`) )
-ENGINE = InnoDB;
+--
+-- Structure de la table `facture`
+--
 
+CREATE TABLE `facture` (
+  `idfacture` int NOT NULL,
+  `designation` varchar(45) DEFAULT NULL,
+  `quantité` float DEFAULT NULL,
+  `prixUnitaire` float DEFAULT NULL,
+  `prixTotal` float DEFAULT NULL,
+  `Total` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- -----------------------------------------------------
--- Table `BD_Informatique`.`tache`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `BD_Informatique`.`tache` (
-  `idtache` INT NOT NULL AUTO_INCREMENT ,
-  `nomTache` VARCHAR(45) NULL ,
-  `assigneA` VARCHAR(45) NULL ,
-  `dateDassignation` DATE NULL ,
-  PRIMARY KEY (`idtache`) )
-ENGINE = InnoDB;
+--
+-- Déchargement des données de la table `facture`
+--
 
+INSERT INTO `facture` (`idfacture`, `designation`, `quantité`, `prixUnitaire`, `prixTotal`, `Total`) VALUES
+(1, 'ordinateur ', 1, 50000, 50000, 50000);
 
+-- --------------------------------------------------------
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `idproduit` int NOT NULL,
+  `nomProduit` varchar(45) DEFAULT NULL,
+  `dateDexpiration` date DEFAULT NULL,
+  `typeDeProduit` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`idproduit`, `nomProduit`, `dateDexpiration`, `typeDeProduit`) VALUES
+(1, 'encre canon', '2022-05-20', 'consommable');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+CREATE TABLE `role` (
+  `idrole` int NOT NULL,
+  `nomRole` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`idrole`, `nomRole`) VALUES
+(1, 'Administrateur'),
+(2, 'Secretaire_Général');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stock`
+--
+
+CREATE TABLE `stock` (
+  `idstock` int NOT NULL,
+  `nomDuProduit` varchar(45) DEFAULT NULL,
+  `quantité` varchar(45) DEFAULT NULL,
+  `dateDajout` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tache`
+--
+
+CREATE TABLE `tache` (
+  `idtache` int NOT NULL,
+  `nomTache` varchar(45) DEFAULT NULL,
+  `assigneA` varchar(45) DEFAULT NULL,
+  `dateDassignation` date DEFAULT NULL,
+  `description` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `tache`
+--
+
+INSERT INTO `tache` (`idtache`, `nomTache`, `assigneA`, `dateDassignation`, `description`) VALUES
+(1, 'ajouter nouveau utilisateur', 'administrateur', '2021-12-02', 'le travail consiste a créer un compte utilisateur pour notre nouveau employer. Nom : nassir\r\nprenom : ahmat\r\nposte : secretaire\r\nlogin : nassirA\r\npass: 1234');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Utilisateur`
+--
+
+CREATE TABLE `Utilisateur` (
+  `idUtilisateur` int NOT NULL,
+  `nom` varchar(45) DEFAULT NULL,
+  `prenom` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  `login` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `Utilisateur`
+--
+
+INSERT INTO `Utilisateur` (`idUtilisateur`, `nom`, `prenom`, `email`, `role`, `login`, `password`) VALUES
+(1, 'elcaskerito', 'ritodev', 'elcaskerito@gmail.com', '1', 'idi', '12344');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`idclient`);
+
+--
+-- Index pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD PRIMARY KEY (`idfacture`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`idproduit`);
+
+--
+-- Index pour la table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`idrole`);
+
+--
+-- Index pour la table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`idstock`);
+
+--
+-- Index pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD PRIMARY KEY (`idtache`);
+
+--
+-- Index pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  ADD PRIMARY KEY (`idUtilisateur`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `idclient` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `facture`
+--
+ALTER TABLE `facture`
+  MODIFY `idfacture` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `idproduit` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `role`
+--
+ALTER TABLE `role`
+  MODIFY `idrole` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `idstock` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `tache`
+--
+ALTER TABLE `tache`
+  MODIFY `idtache` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `Utilisateur`
+--
+ALTER TABLE `Utilisateur`
+  MODIFY `idUtilisateur` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
